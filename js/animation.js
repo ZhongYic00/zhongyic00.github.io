55f8f2ce6b2f9a0b9dc45b63aecb8ad5c12d4663
ZhongYic <zhong200234@outlook.com>
2018-08-12 21:54:58 +0800

    update180812 modified some details

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,7 @@
 var cnt = 0, flag = false, hideflag = false, Topflag = false, Menuflag=false, nightshift = false;
-var a, b, last, lineH, fontH;
+var a, b, c, last, lineH, fontH, platform;
+var Bodys=document.getElementsByTagName("body"),Mains=document.getElementsByTagName("main");
+var Body=Bodys[0],Main=Mains[0];
 var TopCanvas = document.getElementById("to-top-symbol"), TopDiv = document.getElementById("to-top"), TopButton = new Array(),MenuDiv=document.getElementById("menu-button"),MenuCanvas=document.getElementById("menu-symbol"),MenuButton=new Array(),Header=document.getElementById("header"),HeaderContext=document.getElementById("header-context"),ThemeDiv=document.getElementById("theme-button"),ThemeCanvas=document.getElementById("theme-symbol");
 TopButton[0] = TopCanvas, TopButton[1] = TopDiv,MenuButton[0]=MenuCanvas,MenuButton[1]=MenuDiv;
 var Menubox=document.createElement("div"),Menulist=document.createElement("ul"),Menucontext=new Array(),TPcxt,TMcxt;
@@ -31,12 +33,51 @@ function Draw_sun(h,w,obj){
     obj.fillStyle='rgb(255,255,0)';
     obj.fill();
 }
-function Draw_button() {
-    Velocity(document.getElementsByTagName("main"),{opacity:1},{duration:"slow",easing:"ease-out",delay:200});
+function set_theme(type) {
+    console.log(Main.style.marginTop);
+    if(type=="mobile"){
+        Main.style.marginTop=0.12*b+"px";
+        Main.style.boxShadow="",Main.style.borderRadius="";
+        var mgW=Math.floor(0.02*c);
+        Main.style.width=c-2*mgW+"px",Main.style.marginLeft=Main.style.marginRight=mgW+"px",Main.style.paddingLeft=Main.style.paddingRight=0;
+        var pres=document.getElementsByTagName("pre");
+        for(var i=0;i<pres.length;i++)
+            pres[i].style.width=c-2*mgW-10+"px",pres[i].style.marginLeft=pres[i].style.marginRight=0;
+        var codes=document.getElementsByTagName("code");
+        for(var i=0;i<codes.length;i++)codes[i].classList.add("mobile");
+        console.log(c-2*mgW,mgW,c);
+    }
+    else {
+        //var now1=new Date();
+        //console.log(now1.getTime());
+        Main.style.marginTop=0.16*b+"px";
+        Main.style.boxShadow="0 0 1px 1px rgba(0,0,0,0.2)";Main.style.borderRadius="3px";
+        var mgW=Math.floor(0.2*c);
+        Main.style.width=c-2*mgW-0.8*mgW+"px",Main.style.marginLeft=Main.style.marginRight=mgW+"px",Main.style.paddingLeft=Main.style.paddingRight=mgW*0.4+"px",Main.style.paddingTop=0.1*mgW+"px",Main.style.marginBottom=mgW*0.5+"px";
+        var pres=document.getElementsByTagName("pre");
+        for(var i=0;i<pres.length;i++)
+            pres[i].style.width=c-3*mgW+"px",pres[i].style.marginLeft=pres[i].style.marginRight=mgW*0.2+"px";
+        var codes=document.getElementsByTagName("code");
+        for(var i=0;i<codes.length;i++)codes[i].classList.remove("mobile");
+        //Velocity(Main,{backgroundColor:'#FFFFF0'},{duration:"fast"});
+        //var now2=new Date();
+        //console.log(now2.getTime());
+    }
+}
+function init() {
     a = document.documentElement.scrollTop || document.body.scrollTop,
     b = (document.documentElement.clientHeight || document.body.clientHeight) / 2,
+    c = (document.documentElement.clientWidth || document.body.clientWidth),
     lineH = b/16, fontH=b/18;
     HeaderContext.style.marginRight=HeaderContext.style.marginLeft=b/8+"px",TopCanvas.height = TopCanvas.width = b / 10, ThemeCanvas.height = ThemeCanvas.width = MenuCanvas.height = MenuCanvas.width = b*0.12;
+    var HeaderTitle;
+    for(var i=0;i<HeaderContext.childNodes.length;i++){
+        if(HeaderContext.childNodes[i].nodeName=="H1"){
+            HeaderTitle=HeaderContext.childNodes[i];
+            break;
+        }
+    }
+    HeaderTitle.style.lineHeight=b*0.03+"px",HeaderTitle.style.fontSize=b*0.08+"px",Header.style.height=0.12*b+"px";
     TPcxt = TopCanvas.getContext("2d"), TMcxt = ThemeCanvas.getContext("2d");
     TPcxt.beginPath();
     TPcxt.moveTo(b / 10 * 0.5, b / 10 * 0.2);
@@ -76,6 +117,18 @@ function Draw_button() {
     }
     //console.log(lineH);
     Menubox.appendChild(Menulist);
+    Velocity(Body,{backgroundColor:'#E8E8E8'},{duration:1200,easing:"ease-in-out"});
+    if(b*2>c){
+            platform="mobile";
+            Main.style.backgroundColor='#E8E8E8';
+            Velocity(Main,{opacity:1},{duration:"normal",delay:500,easing:"ease-out"});
+    }
+    else {
+            platform="PC";
+            Velocity(Main,{backgroundColor:'#FFFFF0',opacity:1},{duration:"normal",delay:200,easing:"ease-out"});
+    }
+    console.log(platform);
+    set_theme(platform);
 }
 function sleep(d){
     var cur=new Date().getTime(), now = cur;
@@ -85,7 +138,11 @@ function sleep(d){
 function clear(obj,cxt){
     cxt.clearRect(0,0,obj.width,obj.height);
 }
-window.onload = Draw_button(), window.onresize = Draw_button();
+window.onload = init();
+window.onresize = function(){
+    init();
+    //console.log("+1");
+};
 //  TopButton.addEventListener("onclick",return_to_top);
 if (a > 100) {
     flag = true, Velocity(Header, { opacity: 0.8 }, { duration: "fast" });
@@ -118,19 +175,18 @@ window.onscroll = function navibar() {
     }
 }
 TopDiv.onclick = function return_to_top() {
-    Velocity(document.body, "scroll", { duration: "slow", easing: "ease-out" });
+    Velocity(Body, "scroll", { duration: "slow", easing: "ease-out" });
     //Velocity(TopButton,{display:"none"},{opacity:0},{duration:"fast"});
     //Topflag=true;
 }
 MenuDiv.onclick = function show_menu() {
     if(!Menuflag)Velocity(MenuButton,{rotateZ:"45deg"},{duration:"fast",easing:"ease-out"}),
-        Velocity(Header,{height:(0.15*b+4*lineH)+"px"},{duration:"fast",begin:function(){HeaderContext.appendChild(Menubox);Velocity(Menubox,{opacity:1,translateY:0},{duration:"fast",easing:"ease-out"});}});
+        Velocity(Header,{height:(0.12*b+3.5*lineH)+"px"},{duration:"fast",begin:function(){HeaderContext.appendChild(Menubox);Velocity(Menubox,{opacity:1,translateY:0},{duration:"fast",easing:"ease-out"});}});
     else Velocity(MenuButton,"reverse",{duration:"fast",easing:"ease-out"}),
         Velocity(Header,{height:0.12*b},{duration:"fast",begin:function(){Velocity(Menubox,{opacity:0,translateY:-b*0.12},{duration:"fast"});},complete:function(){HeaderContext.removeChild(Menubox);}});
     Menuflag=!Menuflag;
 }
 ThemeDiv.onclick = function change(){
-    var Body=document.getElementsByTagName("body"),Main=document.getElementsByTagName("main");
     if(!nightshift)clear(ThemeCanvas,TMcxt),Draw_moon(ThemeCanvas.height,ThemeCanvas.width,TMcxt),Velocity(Body,{ backgroundColor:'#555555'},{duration:"normal", easing:"ease-in-out"}),Velocity(Main,{color:"#FFFFFF"},{duration:"normal",easing:"ease-in-out"});
     else clear(ThemeCanvas,TMcxt),Draw_sun(ThemeCanvas.height,ThemeCanvas.width,TMcxt),Velocity(Body,"reverse"),Velocity(Main,"reverse");
     nightshift=!nightshift;
