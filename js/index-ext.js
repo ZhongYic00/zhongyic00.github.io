9867b9b015c216c57898c7afa603485cdf430a6f
ZhongYic <zhong200234@outlook.com>
2019-06-26 23:01:43 +0800

    beautified index page

--- js/index-ext.js
+++ js/index-ext.js
@@ -1,25 +1,19 @@
-var FrontPage=document.getElementById('front-page'),Sidebar=document.getElementById('sidebar'),HugeTitle=document.getElementsByTagName('h1')[0];
-const K1=0.85;
+const K1=0.75;
 var Last,DocPos,ScreenHeight,SidebarHidden=true;
-Sidebar.style.visibility="hidden";
-window.onscroll = function navibar() {
+const navibar=()=>{
+    let Sidebar=document.getElementById('sidebar');
+    let currentHeight = (document.documentElement.clientHeight || document.body.clientHeight),
+        currentWidth = (document.documentElement.clientWidth || document.body.clientWidth);
+    if(currentHeight>currentWidth)return;
     Last=DocPos,
     DocPos = document.documentElement.scrollTop || document.body.scrollTop,
     ScreenHeight = (document.documentElement.clientHeight || document.body.clientHeight);
-    //console.log(cnt++, a, b, last, flag, hideflag);
-    if(DocPos>ScreenHeight*K1&&SidebarHidden) {
-    console.log(DocPos,ScreenHeight,SidebarHidden);
-        Velocity(Sidebar,{opacity:1},{visibility:'visible'}),SidebarHidden=false;
-    }
-    if(DocPos<ScreenHeight*K1&&!SidebarHidden) {
-        console.log(DocPos,ScreenHeight,SidebarHidden,"hide");
-        SidebarHidden=true,Velocity(Sidebar,{opacity:0},{duration:'fast',visibility:'hidden'});
-    }
-}
-function scrollByTag(tagName)
-{
-	Velocity(document.getElementsByTagName(tagName)[0],"scroll",{duration:"slow",easing:"ease-out"});
-}
+    if(DocPos>ScreenHeight*K1)
+        Sidebar.style.visibility='visible';
+    if(DocPos<ScreenHeight*K1)
+        Sidebar.style.visibility='hidden';
+};
+const scrollByTag=(tagName)=>{Velocity(document.getElementsByTagName(tagName)[0],"scroll",{duration:"slow",easing:"ease-out"});}
     var pageLinks=document.getElementsByTagName('main')[0].getElementsByTagName('div');
 const f=(url)=>{window.location.assign(url);};
     for(var i=0;i<pageLinks.length;i++){
@@ -27,3 +21,13 @@ const f=(url)=>{window.location.assign(url);};
             f(this.dataset.url);
         });
     };
+const ff=()=>{
+    let currentHeight = (document.documentElement.clientHeight || document.body.clientHeight),
+        currentWidth = (document.documentElement.clientWidth || document.body.clientWidth);
+    if(currentHeight<currentWidth)document.getElementsByTagName('nav')[0].classList.add('pc'),document.getElementById('sidebar').style.visibility='visible';
+    else document.getElementsByTagName('nav')[0].classList.remove('pc'),document.getElementById('sidebar').style.visibility='hidden';
+    navibar();
+}
+ff();
+window.addEventListener('resize',ff);
+window.addEventListener('scroll',navibar);
