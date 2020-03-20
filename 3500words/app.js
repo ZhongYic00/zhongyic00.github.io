570878a7f17e4c50752aa16ffd3c9866b965f61c
ZhongYic <zhong200234@outlook.com>
2020-03-20 20:58:00 +0800

    fix app.js bugs

--- 3500words/app.js
+++ 3500words/app.js
@@ -4,7 +4,7 @@ var progress = 0;
 function Word(e, c) {
 	this.eng = e.textContent.replace(/\[+.*\]/ig, '').replace(/[^a-z\s]/ig, '').replace(/\s*\s?$/g, '').replace(/\s*\s/g,' '),
 	this.cn = c.textContent.replace(/\s*\s?$/g, ''),
-	this.t = Boolean(e.textContent.search('=') != -1);
+	this.t = Boolean(e.textContent.search('=') != -1||e.textContent.replace(/\[+.*\]/ig, '').search('\\u0028')!=-1);
 }
 var checker = {
 	show: null,
